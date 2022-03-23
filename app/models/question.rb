3f954d27f8a8bbdf2224d39dc16c9d1d9033913e
Kucherjashka <vnoir20@gmail.com>
2022-03-23 22:31:00 +0300

    fix question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,4 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :question, presence: true
-  validates :question, length: {maximum: 255}, allow_nil: false, on: :create
-end
+  validates :question, length: { minimum: 3, maximum: 255 }, allow_nil: false
