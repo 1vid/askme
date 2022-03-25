7544cb077a39aa38f31da134e17b9b302a696dcb
Kucherjashka <vnoir20@gmail.com>
2022-03-25 21:55:54 +0300

    add_index_to_users_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_130850) do
+ActiveRecord::Schema.define(version: 2022_03_25_185017) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
