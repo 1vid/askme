3d7a24ecd4fe04274ff2529e89260b382a211da8
Kucherjashka <vnoir20@gmail.com>
2022-03-26 14:22:56 +0300

    add sklonjator in app helper for coorect displaing of question number for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,25 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonjator(questions)
+    number = questions.size 
+
+    one = 'вопрос'
+    few = 'вопроса'
+    many = 'вопросов'
+
+    with_number = false
+
+    return "Этому челу задали #{number} #{many}" if (number % 100).between?(11, 14)
+
+    word = case number % 10
+            when 1 then one
+            when 2..4 then few
+            else
+              many
+            end
+
+    "Этому челу задали #{number} #{word}"
+  end
 end
