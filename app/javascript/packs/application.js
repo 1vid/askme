f57deec2bed86dcf8d4dd7655b8ef61e8a9cfa5b
Kucherjashka <vnoir20@gmail.com>
2022-03-26 12:34:24 +0300

    add script to askme_button for monitor less than 640px

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,17 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  const askButton = document.getElementById('ask-button')
+  const askForm = document.getElementById('ask-form')
+
+  askButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    setTimeout(() => {
+      const displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+      askForm.style.display = displayStyle
+    }, 300)
+  })
+})
