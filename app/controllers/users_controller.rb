8dd7a681b17c4023ccbfb8891efc774031a911ca
Kucherjashka <vnoir20@gmail.com>
2022-03-29 21:02:12 +0300

    blow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: t('.notice  ')
+      redirect_to user_path(@user), notice: t('.notice')
     else
       render 'edit'
     end
