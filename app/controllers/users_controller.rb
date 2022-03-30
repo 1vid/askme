f3b926a3eaf5468e9782701e5287ee50c4a62d95
Kucherjashka <vnoir20@gmail.com>
2022-03-30 05:06:54 +0300

    add auto authenticate after authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
     if @user.save
       redirect_to root_url, notice: t('.notice')
+      session[:user_id] = @user.id
     else
       render 'new'
     end
