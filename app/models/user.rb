d57a8f2e8b7b50a2991ad5cc7653d2fe6772714f
Kucherjashka <vnoir20@gmail.com>
2022-04-04 19:41:41 +0300

    Fixed color validation regex, del method backround_color, edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,21 @@ class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
   EMAIL_VAL_REGEXP = URI::MailTo::EMAIL_REGEXP
-  BACKGROUND_COLOR_REGEX = /\A(#\h+)\z/
+  BACKGROUND_COLOR_REGEX = /\A#\h{3}{1,2}\z/
   USERNAME_REGEX = /\A\w{1,40}\z/
-  DEFAULT_BACKGROUND_COLOR = '#5F9EA0'
-  
+
   attr_accessor :password
 
   before_validation :downcase_username_email
 
   validates :username, format: { with: USERNAME_REGEX }
-
   validates :email, format: { with: EMAIL_VAL_REGEXP }
-
-  validates :email, :username, 
+  validates :email, :username,
             presence: true,
             uniqueness: true
-
   validates :password, presence: true, on: :create
   validates :password, confirmation: true
-  validates :bg_color, format: {with: BACKGROUND_COLOR_REGEX}
+  validates :bg_color, format: { with: BACKGROUND_COLOR_REGEX }
 
   before_save :encrypt_password
 
@@ -40,19 +36,12 @@ class User < ApplicationRecord
     end
   end
 
-  def background_color
-    bg_color
-  end
-
   private
 
   def encrypt_password
     if self.password.present?
-      #создаем "соль" — рандомная строка усложняющая задачу хацкерам
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
-      # создаем хэш пароля — длинная уникальная строка, из которой невозможно восстановить
-      # исходный пароль
       self.password_hash = User.hash_to_string(
         OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
       )
