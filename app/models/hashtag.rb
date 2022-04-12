e64091804b2d0cacf300da33b51301a53cdb0ad1
Kucherjashka <vnoir20@gmail.com>
2022-04-12 23:32:52 +0300

    add join table, add dependences

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,6 @@
 class Hashtag < ApplicationRecord
   HASH_TAG_REGEX = /#[[:word:]-]+/
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :questions, through: :hashtag_questions
   validates :text, presence: true, uniqueness: true
 end
